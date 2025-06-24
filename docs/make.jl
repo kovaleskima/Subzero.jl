639c629e491a46b2fff893e4860b3351821f5eb4
Skylar A Gering <skygering@gmail.com>
2025-06-24 12:18:29 -0400

    Example Cleanup (#113)
    * Moved shear flow and simple strait to literate examples
    
    * Remove repeative examples
    
    * Clean up examples and try to have gif save
    
    * Added four working examples to the webpage
    
    * Finish cleanup of examples and adding to the documentation website.

--- docs/make.jl
+++ docs/make.jl
@@ -4,21 +4,32 @@ import GeoInterface as GI
 import GeometryOps as GO
 import LibGEOS as LG
 
+println(pwd())
+
+function jl_to_md(input, output)
+    # turns .jl files in input to .md files in output
+    for ipath in readdir(input, join = true)
+        # ignore non julia files
+        splitext(ipath)[2] == ".jl" || continue
+        # # full path to a literate script
+        # ipath = joinpath(root, file)
+        # generated output path
+        opath = splitdir(replace(ipath, input=>output))[1]
+        # generate the markdown file calling Literate
+        Literate.markdown(ipath, opath)
+    end
+end
 
 # Converting any files in the literate folder to markdown
-LITERATE_INPUT = joinpath(@__DIR__, "literate")
-LITERATE_OUTPUT = joinpath(@__DIR__, "src")
+println("Building tutorial...")
+tutorial_input = joinpath(@__DIR__, "literate")
+tutorial_output = joinpath(@__DIR__, "src")
+jl_to_md(tutorial_input, tutorial_output)
 
-for (root, _, files) ∈ walkdir(LITERATE_INPUT), file ∈ files
-    # ignore non julia files
-    splitext(file)[2] == ".jl" || continue
-    # full path to a literate script
-    ipath = joinpath(root, file)
-    # generated output path
-    opath = splitdir(replace(ipath, LITERATE_INPUT=>LITERATE_OUTPUT))[1]
-    # generate the markdown file calling Literate
-    Literate.markdown(ipath, opath)
-end
+println("Building examples...")
+examples_input = joinpath(@__DIR__, "literate", "examples")
+examples_output = joinpath(@__DIR__, "src", "examples")
+jl_to_md(examples_input, examples_output)
 
 # Documentation formatting
 format = Documenter.HTML(;
@@ -39,6 +50,13 @@ makedocs(;
     pages=[
         "Introduction" => "index.md",
         "Tutorial" => "tutorial.md",
+        "Examples" => [
+            "examples/shear_flow.md",
+            "examples/simple_strait.md",
+            "examples/forcing_contained_floes.md",
+            "examples/moving_bounds.md",
+            "examples/restart_sim.md"
+        ],
         "API Reference" => "api.md",
         "Improving Subzero" => [
             "Contributing" => "contribute.md",
@@ -46,6 +64,7 @@ makedocs(;
         ],
     ],
     warnonly = true,
+    clean = false,  # TODO: remove before pushing to production
 )
 
 deploydocs(;
